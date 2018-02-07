2ddeedcfe16086105856c67d3e75bbec5948ed7f
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-07 16:17:37 +0530

    Create HIGH LOW.C
    print high or low value

---
+++ HIGH LOW.C
@@ -0,0 +1,30 @@
+include<stdio.h>
+#unclude<conio.h>
+int main()
+{
+int i,j,n=3,a[3],temp=0,max,min;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<3;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<3;i++)
+    {
+    for(j=i+1;j<3;j++)
+    {
+        if(a[i]>a[j])
+        {
+    temp=a[i];
+    a[i]=a[j];
+    a[j]=temp;
+    }
+}
+}
+max=a[2];
+printf("%d",max);
+min=a[0];
+printf("%d",min);
+return 0;
+}
+© 2018 GitHub
