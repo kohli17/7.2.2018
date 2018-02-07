e68207555fd35318a93e98d44029a14410d26dee
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-07 16:14:27 +0530

    Create STRCAT.C
    add two string

---
+++ STRCAT.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[10];	char b[10];
+	printf("enter string 1\n");
+    scanf(#"%s",&a);
+	printf("enter string 2\n");
+	scanf("%s",&b);
+	printf("concatenated strings:%s%s",a,b);
+}
+© 2018 G
